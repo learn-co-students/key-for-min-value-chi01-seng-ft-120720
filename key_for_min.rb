5f8670292380b621a027a65cf6e42ddef2b7c259
Daniel Calderon <danieleduardocald@gmail.com>
2020-11-29 00:01:49 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,26 @@
 
 def key_for_min_value(name_hash)
 
-end
+final_price = []
+  counter = 0
+  best_price = {}
+  
+  for counter in 0.. 10000 do 
+    name_hash.each { |item, price|
+     
+     if counter == price
+       best_price [item] = price
+     end
+    }
+   
+   end
+  
+  counter += 1
+  i = 0
+  for i in 0..0
+     best_price.each { |item, price|
+        final_price<< item
+     }
+    end
+    return final_price[0]
+  end
